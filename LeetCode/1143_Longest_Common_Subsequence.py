f61ec8eb3b4567040bc27d12445251056daefcdd
Derek Lee <dyklee89@gmail.com>
2022-03-29 09:28:53 -0400

    practicing dynamic programming

--- LeetCode/1143_Longest_Common_Subsequence.py
+++ LeetCode/1143_Longest_Common_Subsequence.py
@@ -1,5 +1,20 @@
 def longestCommonSubsequence(text1, text2):
-    ",".join(text1)
-    for char in text2:
-        if char in text1:
-            print(True)
+    dp = [[0 for j in range(len(text2)+1)] for i in range(len(text1)+1)]
+    # print(dp)
+    for i in range(len(text1)-1,-1,-1):
+        for j in range(len(text2)-1, -1, -1):
+            if text1[i] == text2[j]:
+                dp[i][j] = 1+ dp[i+1][j+1]
+            else:
+                dp[i][j] = max(dp[i][j+1], dp[i+1][j])
+    return dp[0][0]
+
+
+    
+
+
+
+a = "abcde"
+b = "def"
+
+print(longestCommonSubsequence(a,b))
