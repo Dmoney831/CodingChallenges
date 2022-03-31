f7482cd46914f7fa27b6a412a3ec8489276c68ad
Derek Lee <dyklee89@gmail.com>
2022-03-31 09:51:57 -0400

    39 combination_sum backtracking and combination

--- LeetCode/139_Word_Break.py
+++ LeetCode/139_Word_Break.py
@@ -2,6 +2,7 @@ def wordBreak(s, wordDict):
     dp = [True] + [False]*len(s)
     for i in range(1, len(s)+1):
         for word in wordDict:
+            print(f"i: {i}, word: {word}, s[:i]: {s[:i]}, s[:i].endswith(word): {s[:i].endswith(word)}")
             if dp[i - len(word)] and s[:i].endswith(word):
                 dp[i] = True
     return dp[-1]
@@ -12,7 +13,7 @@ s = "leetcode"
 wordDict = ["leet","code"]
 s = "applepenapple" 
 wordDict = ["apple","pen"]
-s = "catsandog" 
-wordDict = ["cats","dog","sand","and","cat"]
+# s = "catsandog" 
+# wordDict = ["cats","dog","sand","and","cat"]
 
 print(wordBreak(s, wordDict))
---
+++ LeetCode/39_Combination_Sum.py
@@ -0,0 +1,36 @@
+'''
+iterate over the candiates value and append candidates[i].
+same value can be used unlimited.
+backtracking + combination
+'''
+
+
+def combinationSum(candidates, target):
+    res = []
+    
+    def depth_for_search(i, cur, total):
+        if total == target:
+            res.append(cur.copy())
+            return
+        if i >= len(candidates) or total > target:
+            return
+
+        cur.append(candidates[i])
+        depth_for_search(i, cur, total + candidates[i])
+        cur.pop()
+        depth_for_search(i+1, cur, total)
+
+    depth_for_search(0, [], 0)
+    return res
+
+    
+
+
+
+
+candidates = [2,3,6,7] 
+target = 7
+candidates = [2,3,5] 
+target = 8
+candidates = [2] 
+target = 1
