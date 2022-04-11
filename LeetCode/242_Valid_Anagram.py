270dd10bea661910596b4871e798bee9da2f13be
Derek Lee <dyklee89@gmail.com>
2022-04-10 20:00:26 -0400

    anagram

---
+++ LeetCode/242_Valid_Anagram.py
@@ -0,0 +1,33 @@
+def isAnagram(s,t):
+    ss, tt = len(s), len(t)
+    if ss != tt :
+        return False
+    d1 = {}
+    d2 = {}
+    for i in s:
+        if i in d1:
+            d1[i] += 1
+        else:
+            d2[i] = 1
+    for i in t:
+        if i in d2:
+            d2[i] += 1
+        else:
+            d2[i] = 1
+    for key in d1:
+        if key not in d2 or d1[key] != d2[key]:
+            return False
+    return True
+
+from collections import Counter
+
+def are_anagrams(s, t):
+    if len(s) != len(t):
+        return False
+    return Counter(s) == Counter(t)
+
+'''
+Counter example =>
+{ 'a':1, 'p':2, 'l':1, 'e':1 } 
+
+'''
--- LeetCode/Dynamic_Programming.py
+++ LeetCode/Dynamic_Programming.py
@@ -1,7 +1,6 @@
+######Fibonacci#####
 def fib(n):
-    
     ans = [0 for i in range(n+1)]
-    # print(ans)
     ans[1], ans[2] = 1, 1
 
     for i in range(3, n+1):
@@ -10,10 +9,9 @@ def fib(n):
 
     print(ans[n])
     
-fib(3)
-fib(4)
-fib(5)
-fib(6)
-fib(7)
-fib(8)
 fib(50)
+
+'''
+gridTraveler(grid):
+
+'''
