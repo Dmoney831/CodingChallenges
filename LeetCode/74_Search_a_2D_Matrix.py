84b024f7e6af58da888523cc926655a2bef6ddde
Derek Lee <dyklee89@gmail.com>
2022-05-10 00:28:57 -0400

    leet 981

--- LeetCode/74_Search_a_2D_Matrix.py
+++ LeetCode/74_Search_a_2D_Matrix.py
@@ -6,6 +6,38 @@ def searchMatrix(matrix, target):
 
 
 
+def searchMatrix(matrix, target):
+    m = len(matrix)
+    n = len(matrix[0])
+    top, bottom = 0, m - 1
+    while top <= bottom:
+        row = (top + bottom) // 2
+        if target > matrix[row][-1]:
+            top = row + 1
+        elif target < matrix[row][0]:
+            bottom = row - 1
+        else:
+            break
+    # if target is not in any rows in while loop
+    if not (top <= bottom):
+        return False
+    
+    # check target in a specific row
+    row = (top + bottom) // 2
+    left, right = 0, n - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if target > matrix[row][mid]:
+            left = mid + 1
+        elif target < matrix[row][mid]:
+            right = mid - 1
+        else:
+            return True
+    return False
+
+
+
+
 
 matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]] 
 target = 3
