946fdde3594dc19f3567e2bfb0813c7300274512
Derek Lee <dyklee89@gmail.com>
2022-05-14 18:43:19 -0400

    leet 55

--- LeetCode/53_Maximum_Subarray.py
+++ LeetCode/53_Maximum_Subarray.py
@@ -1,5 +1,5 @@
 def maxSubArray(nums):
-    max_sub = nums[0]
+    max_sub = 0
     sum = 0
 
     for i in nums:
@@ -10,9 +10,20 @@ def maxSubArray(nums):
         max_sub = max(max_sub, sum)
     return max_sub 
 
+def maxSubArray(nums):
+    max_sub = 0
+    total = 0
+    for i in nums:
+        if total < 0:
+            total = 0
+        total += i
+        max_sub = max(total, max_sub)
+    return max_sub
+
 a = [-2,1,-3,4,-1,2,1,-5,4]
 b = [1]
 c = [5,4,-1,7,8]
 d = [-1, -4, -5, -3, -7]
 
 print(maxSubArray(a))
+# ********Time Complexity: Big O(n)
--- LeetCode/55_Jump_Game.py
+++ LeetCode/55_Jump_Game.py
@@ -1,47 +1,39 @@
-'''
-Array is given which is consists of positive numbers.
-always start with idx 0 and jump to dix 1. 
-if nums[n-i-1] > nums[n-i] then stick with [n-i-1] so that I can jumps more. 
-
-'''
-
-# def canJump(nums):
-#     z = len(nums)-1
-#     if len(nums) == 1:
-#         return True
-#     for i in range(0, z):
-#         if nums[i] < z:
-#             z = z-1
-#     return True if z != 0 else False
-
+def canJump(nums):
+    jump = 0
+    for idx, num in enumerate(nums):
+        print(f'idx: {idx}, num: {num}, jump: {jump}')
+        if idx > jump:
+            return False
+        jump = max(jump, idx + num)
+    return True
 
         
-a = [2,3,1,1,4]
-b = [3,2,1,0,4]
-c = [0]
-# print(canJump(c))
+nums = [2,3,1,1,4] # True
+# nums = [3,2,1,0,4] # False
+# nums = [0] # True
+print(canJump(nums))
 
-def canJump0(nums):
-    n = len(nums)
-    if n <= 1:
-        return True
-    dp = [True] + [False]*(n-1)
-    for i in range(1,n):
-        dp[i] = any(nums[j] >= (i-j) for j in range(i) if dp[j])
-    return dp[n-1]
+# def canJump0(nums):
+#     n = len(nums)
+#     if n <= 1:
+#         return True
+#     dp = [True] + [False]*(n-1)
+#     for i in range(1,n):
+#         dp[i] = any(nums[j] >= (i-j) for j in range(i) if dp[j])
+#     return dp[n-1]
 
-    '''
-    dynamic programming
-    time complexity is O(n^2)
-    '''
+'''
+dynamic programming
+time complexity is O(n^2)
+'''
 
-def canJunp1(nums):
-    goal = len(nums) -1
-    for i in range(len(nums) -1, -1, -1):
-        print(i, nums[i])
-        if i + nums[i] >= goal:
-            goal = i
-    return True if goal == 0 else False
+# def canJunp1(nums):
+#     goal = len(nums) -1
+#     for i in range(len(nums) -1, -1, -1):
+#         print(i, nums[i])
+#         if i + nums[i] >= goal:
+#             goal = i
+#     return True if goal == 0 else False
 
 '''
 time complexity is O(n)
