04b9eaa3b72caedb0874f886c173c2c5bbf304de
Derek Lee <dyklee89@gmail.com>
2022-05-13 23:36:21 -0400

    leet 973

---
+++ LeetCode/973_K_Closest_Points_to_Origin.py
@@ -0,0 +1,25 @@
+from cmath import sqrt
+import heapq
+def kClosest(points, k):
+    heap = []
+    for x,y in points:
+        if len(heap)<k:
+            print(heap)
+            heapq.heappush(heap,[-(x*x+y*y),[x,y]])
+            print(heap)
+        else:
+            heapq.heappushpop(heap,[-(x*x+y*y),[x,y]])
+            # print(heap)
+    return [pair for value, pair in heap]
+    
+
+
+
+points = [[1,3],[-2,2]] 
+k = 1
+# Output: [[-2,2]]
+
+points = [[3,3],[5,-1],[-2,4]] 
+k = 2
+# Output: [[3,3],[-2,4]]
+print(kClosest(points, k))
