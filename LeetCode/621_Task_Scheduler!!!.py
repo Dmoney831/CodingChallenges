61f40ed7e29f037fd7bbbd96cc994153e928c0e7
Derek Lee <dyklee89@gmail.com>
2022-05-14 17:25:54 -0400

    leet 621

--- LeetCode/621_Task_Scheduler!!!.py
+++ LeetCode/621_Task_Scheduler!!!.py
@@ -8,19 +8,20 @@ def leastInterval(tasks, n):
     print(count, room, idle)
     while count and idle > 0:
         idle -= min(room, count.pop())
-        print(f'idle: {idle}')
-    print(count)
+        print(f'count: {count}, idle: {idle}')
+    # print(count)
     return max(0, idle) + len(tasks)
 
 tasks = ["A","A","A","B","B","B"] 
-n = 2
+n = 2 
 # Output: 8
-tasks = ["A","A","A","B","B","B"] 
-n = 0
+# tasks = ["A","A","A","B","B","B"] 
+# n = 0 
 # Output: 6
 tasks = ["A","A","A","A","A","A","B","C","D","E","F","G"]
 n = 2
 # Output: 16
+
 
 print(leastInterval(tasks, n))
 # *****Time Complexity: O(n)*******
