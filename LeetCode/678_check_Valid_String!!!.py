6cd82d0ba803e28fbc0e45901c690f1f0c594d3b
Derek Lee <dyklee89@gmail.com>
2022-05-17 12:05:48 -0400

    leet 678

--- LeetCode/678_check_Valid_String!!!.py
+++ LeetCode/678_check_Valid_String!!!.py
@@ -1,19 +1,18 @@
 def checkValidString(s):
     leftMin, leftMax = 0, 0
     for i in s:
-        if i == '(':
+        if i == "(":
             leftMin += 1
             leftMax += 1
-        if i == ')':
+        if i == ")":
             leftMin = max(leftMin - 1, 0)
             leftMax -= 1
-        if i == '*':
+        if i == "*":
             leftMin = max(leftMin - 1, 0)
             leftMax += 1
         if leftMax < 0:
             return False
-    return leftMin == 0
-
+        return leftMin == 0
 
 s = "()"
 # Output: true
