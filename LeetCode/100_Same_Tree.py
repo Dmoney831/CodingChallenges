3588a9984b5f8de9e0f29a2879504aac64221698
Derek Lee <dyklee89@gmail.com>
2022-05-19 22:56:39 -0400

    leet 100

--- LeetCode/100_Same_Tree.py
+++ LeetCode/100_Same_Tree.py
@@ -8,3 +8,11 @@ class Solution:
             return False
         
         return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p is q
+
+# Time complexity: O(n) where n is the number of the nodes of both trees.
