8424473fa8770f28ebe15b9cfbd0cc40f16afac7
Derek Lee <dyklee89@gmail.com>
2022-05-20 07:00:53 -0400

    leet 235

--- LeetCode/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
+++ LeetCode/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -9,3 +9,15 @@ class Solution:
                 cur = cur.left
             else:
                 return cur
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            if p.val < root.val > q.val:
+                root = root.left
+            elif p.val > root.val < q.val:
+                root = root.right
+            else:
+                return root
+# Time Complexity: O(H), where H is the height of Binary Tree.
+# Space Complexity: O(1)
