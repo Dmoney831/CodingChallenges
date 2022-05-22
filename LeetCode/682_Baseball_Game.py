8024f4ef99c28b093dc2b3e68566ed32a9addb5e
Derek Lee <dyklee89@gmail.com>
2022-05-21 20:12:37 -0400

    682

---
+++ LeetCode/682_Baseball_Game.py
@@ -0,0 +1,13 @@
+# + -> is the sum of the previous two scores.
+# D -> *2 the previous score
+# C -> remove previous socre
+
+def calPoints(ops):
+    
+
+
+
+ops = ["5","2","C","D","+"]
+# Output: 30
+
+print(calPoints(ops))
