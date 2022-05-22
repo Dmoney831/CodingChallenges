7cf09561ace50a65d151107bf689ca592222a451
Derek Lee <dyklee89@gmail.com>
2022-05-22 18:39:40 -0400

    2

--- Object_Oriented_Programming_Python/main1.py
+++ Object_Oriented_Programming_Python/main1.py
@@ -1,16 +1,43 @@
 class Item:
-    def cal_total_price(self, x, y):
-        return x * y
+    pay_rate = 0.8 # The pay rate after 20% discount
+    def __init__(self, name: str, price: float, quantity=0):
+        # Run validations to the received arguemtns
+        assert price >= 0, f"Price {price} is not greater or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
+        # print(f'An instance created: {name}')
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        
+
+    def cal_total_price(self):
+        return self.price * self.quantity
+    
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
+print(Item.pay_rate)
+# print(Item.__dict__) # All the attributes for Class Level
 
 
-item1 = Item()
-item1.name = 'Phone'
-item1.price = 100
-item1.quantity = 5
-print(item1.cal_total_price(item1.price, item1.quantity))
+item1 = Item('Phone', 100, 1)
+print(item1.__dict__) # All the attributes for instance level
+# print(item1.name)
+# print(item1.price)
+# print(item1.quantity)
+print(item1.pay_rate)
+print(item1.cal_total_price())
+item1.apply_discount()
+print(item1.price)
 
-item2 = Item()
-item2.name = 'Laptop'
-item2.price = 1000
-item2.quantity = 3
-print(item2.cal_total_price(item2.price, item2.quantity))
+item2 = Item('Laptop', 1000, 3)
+print(item2.__dict__)
+# print(item2.name)
+# print(item2.price)
+# print(item2.quantity)
+print(item2.pay_rate)
+print(item2.cal_total_price())
+item2.pay_rate = 0.7
+item2.apply_discount()
+print(item2.price)
