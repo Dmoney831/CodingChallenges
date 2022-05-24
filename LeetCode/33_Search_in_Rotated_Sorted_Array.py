c6cb1158e68b37ea19b749555ae0e9577ae93f53
Derek Lee <dyklee89@gmail.com>
2022-05-23 21:52:06 -0400

    leet 33

--- LeetCode/33_Search_in_Rotated_Sorted_Array.py
+++ LeetCode/33_Search_in_Rotated_Sorted_Array.py
@@ -33,23 +33,21 @@ b = 0
 
 print(search(a,b))
 
-# def search(nums, target):
-#     left = 0
-#     right = len(nums) - 1
-
-#     while left <= right:
-#         middle = (left + right)//2
-#         if target == nums[middle]:
-#             return middle
-#         # left sorted
-#         if target > nums[middle] or target < nums[left]:
-#             left = middle + 1
-#         else:
-#             right = middle -1
-#         # right sorted
-#         if target < nums[middle] or target > nums[right]:
-#             right = middle -1
-#         else:
-#             left = middle + 1
-#     return -1
+def search(nums, target):
+    left, right = 0 , len(nums) - 1
     
+    while left <= right:
+        mid = (left + right) // 2
+        if target == nums[mid]:
+            return mid
+        if nums[left] <= nums[mid]:
+            if target > nums[mid] or target < nums[left]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        if nums[mid] <= nums[right]:
+            if target < nums[mid] or target > nums[right]:
+                right = mid - 1
+            else:
+                left = mid + 1
+    return -1
