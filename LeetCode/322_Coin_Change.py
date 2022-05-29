b5921196a2a69aebc35b7666ee1facc8c2876dac
Derek Lee <dyklee89@gmail.com>
2022-05-29 01:48:19 -0400

    leet 322

--- LeetCode/322_Coin_Change.py
+++ LeetCode/322_Coin_Change.py
@@ -7,13 +7,23 @@ def coinChange(coins, amount):
                 dp[a] = min(dp[a], 1+dp[a-c])
     return dp[amount] if dp[amount] != amount+1 else -1
 
+def coinChange(coins, amount):
+    dp = [0] + [float('inf') for i in range(amount)]
+    for i in range(1, amount + 1):
+        for coin in coins:
+            if i - coin >= 0:
+                # print(f'dp[i-coin] = {dp[i-coin]}, dp{i} = {dp[i]}')
+                dp[i] = min(dp[i], dp[i-coin] + 1)
+    print(dp)
+    if dp[-1] == float('inf'):
+        return -1
+    return dp[-1]
 
 
+coins = [1,2,5]
+amount = 11
 
-a = [1,2,5]
-b = 11
-
-print(coinChange(a,b))
+print(coinChange(coins, amount))
 # coins = [1,2,5]
 # coins.sort()
 # coins = coins[::-1]
