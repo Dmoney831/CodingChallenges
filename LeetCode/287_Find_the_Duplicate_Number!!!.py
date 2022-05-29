45f08408dbf3428ee6563fb04dfcf5f6732f9c4e
Derek Lee <dyklee89@gmail.com>
2022-05-28 23:09:17 -0400

    leet 287

--- LeetCode/287_Find_the_Duplicate_Number!!!.py
+++ LeetCode/287_Find_the_Duplicate_Number!!!.py
@@ -14,6 +14,23 @@ def findDuplicate(nums):
         
     # return nums[slow]
 
+def findDuplicate(nums):
+    low = 1
+    high = len(nums) - 1
+    while low < high: 
+        mid = low+(high-low)/2
+        count = 0
+        for i in nums: 
+            if i <= mid:
+                count += 1
+        if count <= mid:
+            low = mid+1
+        else:
+            high = mid
+    return low
+
+
+
 
 
 
