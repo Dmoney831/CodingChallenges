dac9ae3f745648d20ea3f52c65949a52c3086585
Derek Lee <dyklee89@gmail.com>
2022-05-29 01:22:03 -0400

    leet 300

--- LeetCode/300_Longest_Increasing_Subsequence.py
+++ LeetCode/300_Longest_Increasing_Subsequence.py
@@ -8,9 +8,20 @@ def lengthOfLIS(nums):
             # print(nums[j])
             if nums[i] < nums[j]:
                 LIS[i] = max(LIS[i], 1+LIS[j])
-    return max(LIS)
 
+    return max(LIS) 
 
+def lengthOfLIS(nums):
+    n = len(nums)
+    dp = [1] * n
+    for i in range(n):
+        for j in range(i):
+            # print(nums[i], nums[j], dp[i], dp[j])
+            if nums[i] > nums[j] and dp[i] < dp[j] + 1:
+                dp[i] = dp[j] + 1
+    # print(nums)
+    # print(dp)
+    return max(dp)
 
 a = [10,9,2,5,3,7,101,18]
 # a = [0,1,0,3,2,3]
