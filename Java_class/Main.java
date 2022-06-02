3546e97014fbf9a344ca2440dae4d0ea961ca61a
Derek Lee <dyklee89@gmail.com>
2022-06-02 14:26:21 -0400

    date

--- Java_class/Main.java
+++ Java_class/Main.java
@@ -1,8 +1,43 @@
+// public class Main {
+    //     public static void main(String[] args) {
+        //         int myAge = 35;
+        //         int herAge = myAge;
+        //         System.out.println(herAge);
+        //     }
+        // }
+        
+        
+        // Primitive Types
+        // *******Type**** Bytes******* Range***********
+        //  byte            1            [-128, 127]
+        //  short           2            [-32K, 32K]
+        //  int             4            [-2B,   2B]
+        //  long            8            
+        //  float           4            
+        //  double          8            
+        //  char            2            A, B, C
+        //  Boolean         1            true/false
+        
+        // public class Main {
+            //     public static void main(String[] args) {
+                //         byte age = 30;
+                //         int viewsCount = 123_456_789;
+                //         long viewsCount1 = 3_123_456_789L;
+                //         float price = 10.99F;
+                //         char letter = 'A';
+                //         boolean isEligible = true;
+                //     }
+                
+                // }
+
+
+
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        int age = 30;
-        System.out.println(age);
+        byte age = 30;
+        Date now = new Date();
+        System.out.println(now);
     }
 }
-
-// 
