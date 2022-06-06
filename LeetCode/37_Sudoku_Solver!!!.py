ad328d03ade6dacc703748e59f81b2ade5b8376b
Derek Lee <dyklee89@gmail.com>
2022-06-06 18:14:44 -0400

    leet 37

--- LeetCode/37_Sudoku_Solver!!!.py
+++ LeetCode/37_Sudoku_Solver!!!.py
@@ -1,9 +1,157 @@
+from collections import defaultdict
+class Solution:
+    def solveSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: void Do not return anything, modify board in-place instead.
+        """
+        def could_place(d, row, col):
+            """
+            Check if one could place a number d in (row, col) cell
+            """
+            return not (d in rows[row] or d in columns[col] or \
+                    d in boxes[box_index(row, col)])
+        
+        def place_number(d, row, col):
+            """
+            Place a number d in (row, col) cell
+            """
+            rows[row][d] += 1
+            columns[col][d] += 1
+            boxes[box_index(row, col)][d] += 1
+            board[row][col] = str(d)
+            
+        def remove_number(d, row, col):
+            """
+            Remove a number which didn't lead 
+            to a solution
+            """
+            del rows[row][d]
+            del columns[col][d]
+            del boxes[box_index(row, col)][d]
+            board[row][col] = '.'    
+            
+        def place_next_numbers(row, col):
+            """
+            Call backtrack function in recursion
+            to continue to place numbers
+            till the moment we have a solution
+            """
+            # if we're in the last cell
+            # that means we have the solution
+            if col == N - 1 and row == N - 1:
+                nonlocal sudoku_solved
+                sudoku_solved = True
+            #if not yet    
+            else:
+                # if we're in the end of the row
+                # go to the next row
+                if col == N - 1:
+                    backtrack(row + 1, 0)
+                # go to the next column
+                else:
+                    backtrack(row, col + 1)
+                
+                
+        def backtrack(row = 0, col = 0):
+            """
+            Backtracking
+            """
+            # if the cell is empty
+            if board[row][col] == '.':
+                # iterate over all numbers from 1 to 9
+                for d in range(1, 10):
+                    if could_place(d, row, col):
+                        place_number(d, row, col)
+                        place_next_numbers(row, col)
+                        # if sudoku is solved, there is no need to backtrack
+                        # since the single unique solution is promised
+                        if not sudoku_solved:
+                            remove_number(d, row, col)
+            else:
+                place_next_numbers(row, col)
+                    
+        # box size
+        n = 3
+        # row size
+        N = n * n
+        # lambda function to compute box index
+        box_index = lambda row, col: (row // n ) * n + col // n
+        
+        # init rows, columns and boxes
+        rows = [defaultdict(int) for i in range(N)]
+        columns = [defaultdict(int) for i in range(N)]
+        boxes = [defaultdict(int) for i in range(N)]
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] != '.': 
+                    d = int(board[i][j])
+                    place_number(d, i, j)
+        
+        sudoku_solved = False
+        backtrack()
+
+import collections
+
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
-        pass
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        n = len(board)
+
+        rows, cols, boxes = collections.defaultdict(set), collections.defaultdict(set), collections.defaultdict(set)
+
+        for r in range(n):
+            for c in range(n):
+                if board[r][c] == '.':
+                    continue
+
+                v = int(board[r][c])
+                rows[r].add(v)
+                cols[c].add(v)
+                boxes[(r // 3) * 3 + c // 3].add(v)
 
 
+        def is_valid(r, c, v):
+            box_id = (r // 3) * 3 + c // 3
+            return v not in rows[r] and v not in cols[c] and v not in boxes[box_id]
 
+
+        def backtrack(r, c):
+            if r == n - 1 and c == n:
+                return True
+            elif c == n:
+                c = 0
+                r += 1
+
+            # current grid has been filled
+            if board[r][c] != '.':
+                return backtrack(r, c + 1)
+
+            box_id = (r // 3) * 3 + c // 3
+            for v in range(1, n + 1):
+                if not is_valid(r, c, v):
+                    continue
+
+                board[r][c] = str(v)
+                rows[r].add(v)
+                cols[c].add(v)
+                boxes[box_id].add(v)
+
+                if backtrack(r, c + 1):
+                    return True
+
+                # backtrack
+                board[r][c] = '.'
+                rows[r].remove(v)
+                cols[c].remove(v)
+                boxes[box_id].remove(v)
+
+            return False
+
+
+        backtrack(0, 0)
 
 
 board = [
